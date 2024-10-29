a1dea06cd5714eb5fc98751459eee81392193cc9
ttrunghieu97 <ttrunghieu97@gmail.com>
2024-10-29 18:26:22 +0700

    fix

--- components/AlbumCarousel.tsx
+++ components/AlbumCarousel.tsx
@@ -1,14 +1,22 @@
-'use client'
+'use client';
 
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
+
+// Define the structure of an album
+interface Album {
+  id: string;
+  coverPhotoBaseUrl: string;
+  title: string;
+}
 
 const AlbumCarousel = () => {
-  const [albums, setAlbums] = useState([]);
+  const [albums, setAlbums] = useState<Album[]>([]); // Use the Album type here
 
   useEffect(() => {
     const fetchAlbums = async () => {
       const response = await fetch('/api/albums');
-      const data = await response.json();
+      const data: Album[] = await response.json();
       setAlbums(data);
     };
 
@@ -18,17 +26,13 @@ const AlbumCarousel = () => {
   return (
     <div className="relative">
       <div className="flex overflow-x-scroll space-x-4">
-        {albums.map(album => (
+        {albums.map((album) => (
           <a
             key={album.id}
-            href={`https://photos.google.com/albums/${album.id}`} // Đường dẫn đến album
+            href={`https://photos.google.com/albums/${album.id}`} // Link to the album
             className="flex-shrink-0 w-48 h-48 bg-gray-200 rounded-lg overflow-hidden"
           >
-            <img
-              src={album.coverPhotoBaseUrl}
-              alt={album.title}
-              className="w-full h-full object-cover"
-            />
+            <Image src={album.coverPhotoBaseUrl} alt={album.title} width={192} height={192} />
             <div className="p-2 text-center">
               <h3 className="text-sm font-semibold">{album.title}</h3>
             </div>
