23c4647cbd9e59c374f9894bafaba43b9ac55b4b
ttrunghieu97 <ttrunghieu97@gmail.com>
2024-10-30 10:22:37 +0700

    Add the Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -36,6 +37,8 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
+
         </ThemeProvider>
       </body>
     </html>
